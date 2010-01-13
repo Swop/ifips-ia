e7bf8fea025c4bbf41c5b1ac3e18dbccdfb01a2e
jeanbaptiste.richardet <jeanbaptiste.richardet@d694707c-001c-11df-9289-67a056c6595c>
2010-01-13 09:16:28 +0000

    UltimIA

---
+++ tablut/UltimIA.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tablut;
+
+/**
+ *
+ * @author NorTicUs
+ */
+public class UltimIA implements IJoueur{
+
+    public void initJoueur(int mycolour) {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String choixMouvement() {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void declareLeVainqueur(int colour) {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouvementEnnemi(int startRow, int startCol, int finishRow, int finishCol) {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String quadriName() {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
