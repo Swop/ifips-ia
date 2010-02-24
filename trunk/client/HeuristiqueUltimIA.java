92cb20d98e91c67c87c817a9a4070beb9b53ee5c
jeanbaptiste.richardet <jeanbaptiste.richardet@d694707c-001c-11df-9289-67a056c6595c>
2010-02-24 16:14:42 +0000

    modif plateau + début heur

--- trunk/client/HeuristiqueUltimIA.java
+++ trunk/client/HeuristiqueUltimIA.java
@@ -10,7 +10,13 @@ package client;
  * @author swop
  */
 public class HeuristiqueUltimIA implements IHeuristique {
-    /**
+    
+	/**
+	 * valeur de l'heuristique pour ce plateau
+	 */	
+	private int eval = 0;
+	
+	/**
      * Evalue le plateau donne en paramettre
      * @param p
      * Plateau de jeu
@@ -18,12 +24,81 @@ public class HeuristiqueUltimIA implements IHeuristique {
      * La valeur heuristique representative du plateau
      */
     public int evalue(Plateau p, int couleur) {
-	// TODO Heuristique
-
-	// ---- Heuristique aleatoire ... En attendant de faire mieux ...
-
-	return (int) (Math.random() * 100);
-	// ----
+    	if(p.getPlaceRoi().getX() == 0 && (p.getPlaceRoi().getY() == 3 || p.getPlaceRoi().getY() == 4 ||
+    			p.getPlaceRoi().getY() == 5)){
+    		if(couleur == ClientJeu.BLANC)
+    			return Integer.MAX_VALUE;
+    		else
+    			return Integer.MIN_VALUE;
+    	}
+    	if(p.getPlaceRoi().getX() == 8 && (p.getPlaceRoi().getY() == 3 || p.getPlaceRoi().getY() == 4 ||
+    			p.getPlaceRoi().getY() == 5)){
+    		if(couleur == ClientJeu.BLANC)
+    			return Integer.MAX_VALUE;
+    		else
+    			return Integer.MIN_VALUE;
+    	}
+    	if(p.getPlaceRoi().getY() == 0 && (p.getPlaceRoi().getX() == 3 || p.getPlaceRoi().getX() == 4 ||
+    			p.getPlaceRoi().getX() == 5)){
+    		if(couleur == ClientJeu.BLANC)
+    			return Integer.MAX_VALUE;
+    		else
+    			return Integer.MIN_VALUE;
+    	}
+    	if(p.getPlaceRoi().getY() == 8 && (p.getPlaceRoi().getX() == 3 || p.getPlaceRoi().getX() == 4 ||
+    			p.getPlaceRoi().getX() == 5)){
+    		if(couleur == ClientJeu.BLANC)
+    			return Integer.MAX_VALUE;
+    		else
+    			return Integer.MIN_VALUE;
+    	}
+    	int nbCoupGagnant = 0;
+    	for(Mouvement m : p.getMouvementsPossiblesPourUnPoint(p.getPlaceRoi())){
+    		if(m.getDest().getX() == 0 && (m.getDest().getY() == 3 || m.getDest().getY() == 4 || m.getDest().getY() == 5)){
+    			nbCoupGagnant++;
+    			if(nbCoupGagnant == 2){
+    				if(couleur == ClientJeu.BLANC)
+    	    			return Integer.MAX_VALUE;
+    	    		else
+    	    			return Integer.MIN_VALUE;
+    			}
+    		}
+    		if(m.getDest().getX() == 8 && (m.getDest().getY() == 3 || m.getDest().getY() == 4 || m.getDest().getY() == 5)){
+    			nbCoupGagnant++;
+    			if(nbCoupGagnant == 2){
+    				if(couleur == ClientJeu.BLANC)
+    	    			return Integer.MAX_VALUE;
+    	    		else
+    	    			return Integer.MIN_VALUE;
+    			}
+    		}
+    		if(m.getDest().getY() == 0 && (m.getDest().getX() == 3 || m.getDest().getX() == 4 || m.getDest().getX() == 5)){
+    			nbCoupGagnant++;
+    			if(nbCoupGagnant == 2){
+    				if(couleur == ClientJeu.BLANC)
+    	    			return Integer.MAX_VALUE;
+    	    		else
+    	    			return Integer.MIN_VALUE;
+    			}
+    		}
+    		if(m.getDest().getY() == 8 && (m.getDest().getX() == 3 || m.getDest().getX() == 4 || m.getDest().getX() == 5)){
+    			nbCoupGagnant++;
+    			if(nbCoupGagnant == 2){
+    				if(couleur == ClientJeu.BLANC)
+    	    			return Integer.MAX_VALUE;
+    	    		else
+    	    			return Integer.MIN_VALUE;
+    			}
+    		}
+    	}
+    	if(couleur == ClientJeu.BLANC)
+    		eval += 3*(p.getMouvementsPossiblesPourUnPoint(p.getPlaceRoi()).size());
+    	else
+    		eval -= 3*(p.getMouvementsPossiblesPourUnPoint(p.getPlaceRoi()).size());
+    	eval += p.getNbMyPions(couleur);
+    	eval -= p.getNbYourPions(couleur);
+    	
+    	return eval;
     }
-
+    //return (int) (Math.random() * 100);
 }
